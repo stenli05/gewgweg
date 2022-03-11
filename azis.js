489372443dd22a20756ffc07111598d67d79842f
stenli05 <stenli.s.hristov.2019@elsys-bg.org>
2022-03-11 18:37:42 +0200

    First main commit
    Added the skeleton of the site.

---
+++ azis.js
@@ -0,0 +1,46 @@
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://Hacktues:<Lo6PvbkvwAOAuC3I>@cluster0.z9xqf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+}
+);
+ 
+class bezdomnicite{
+    constructor(_location , _type );
+
+        await createListing(client,
+    {
+     _location : 57,575.4525
+    _type :
+);
+        await editListing(client,
+    {
+       _location:55
+       _type :"DF"
+    }
+    );
+    await deleteListing(client,
+        {
+           _location :587
+            _type : "DFHD"
+    }
+        };   
+    );
+    const button = document.querySelectorAll("a");
+    buttons.array.forEach(element => {
+        
+    });
+    btn addEventListener(click , function (e));{
+
+        let x = e.clientX = target.offsetUP;
+        let ripples = document.create.element('span');
+         
+
+
+
+    }
